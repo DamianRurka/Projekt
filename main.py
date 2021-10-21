316a49bb1e76c3d156cfadf9fd164623c8fa59c2
Damian Rurka <gladek123x@gmail.com>
2021-10-21 21:26:02 +0200

    kwadracik

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import pygame
+pygame.init()
+x = 10
+box = pygame.Rect(10,10,50,50)
+clock = pygame.time.Clock()
+delta = 0.0
+screen = pygame.display.set_mode((640,420))
+while True:
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit(0)
+
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            sys.exit(0)
+    #Odliczanie?
+    delta += clock.tick()/1000.0
+    while delta > 1/2.0:
+        print("hey")
+        delta -= 1/2.0
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        box.x += 1
+    if keys[pygame.K_s]:
+        box.y += 1
+    if keys[pygame.K_w]:
+        box.y -= 1
+    if keys[pygame.K_a]:
+        box.x -= 1
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen, (0, 150, 255),box)
+    pygame.display.flip()
