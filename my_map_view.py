55f5ea88a4fd5216a8de4e8a4d8bf366ea1ffd55
Damian Rurka <gladek123x@gmail.com>
2021-12-20 20:07:20 +0100

    poprawki

--- my_map_view.py
+++ my_map_view.py
@@ -1,13 +1,11 @@
 from kivy_garden.mapview import MapView
 
-
 class MyMapView(MapView):
-
-    def on_touch_down(self, touch):
+    def on_touch_down(self,touch):
         pass
 
-    def on_touch_up(self, touch):
+    def on_touch_up(self,touch):
         pass
 
-    def on_touch_move(self, touch):
+    def on_touch_move(self,touch):
         pass
